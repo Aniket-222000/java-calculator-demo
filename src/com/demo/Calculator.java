0364aab9503c703ffc2bd9c55081e72185b49490
Aniket-222000 <chakkarwaraniket@gmail.com>
2026-05-04 15:21:07 +0530

    Add addition method to Calculator

--- src/com/demo/Calculator.java
+++ src/com/demo/Calculator.java
@@ -2,6 +2,11 @@ package com.demo;
 
 public class Calculator {
 
+
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the Java Calculator Version 1.0!");
     }
